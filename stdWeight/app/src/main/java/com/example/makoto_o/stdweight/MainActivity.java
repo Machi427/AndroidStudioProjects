44814e18e1f41613183a8b935f0d234b24b22378
Machi427 <machirda427@gmail.com>
2015-10-14 21:20:11 +0900

    体重計計算アプリ作成完了

--- stdWeight/app/src/main/java/com/example/makoto_o/stdweight/MainActivity.java
+++ stdWeight/app/src/main/java/com/example/makoto_o/stdweight/MainActivity.java
@@ -1,52 +1,56 @@
 package com.example.makoto_o.stdweight;
 
+import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class MainActivity extends Activity {
+    //標準体重
+    double stdWeight;
+    //メッセージ用のテキスト
+    String stdWeightTxt;
+    String errorMsg;
+
+    //小数点以下２桁まで表示するフォーマット
+    DecimalFormat myFormat = new DecimalFormat("###.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //文字列リソースを取得
+        Resources res = getResources();
+        stdWeightTxt = res.getString(R.string.stdWeight);
+        errorMsg = res.getString(R.string.erroMsg);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    //ボタンがクリックされた
+    public void onClickButton(View view) {
+        //身長が入力されているEditText
+        EditText heightText = (EditText) findViewById(R.id.heightText);
+        String heightStr = heightText.getText().toString();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //結果を表示するTextView
+        TextView stdWeightTextView = (TextView) findViewById(R.id.stdWeightText);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try {
+            //標準体重を計算する
+            double height = Double.parseDouble(heightStr);
+            stdWeight = Math.pow(height / 100, 2) * 22;
+            String stdWeightStr = myFormat.format(stdWeight);
+            stdWeightTextView.setText(stdWeightTxt + stdWeightStr + "Kg" );
+
+
+
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
