e6f80596824ce0f19c7bbf2f8d392b3955307919
Machi427 <machirda427@gmail.com>
2015-10-18 04:27:56 +0900

    TwoTouchMail追加

--- MultiActivity/app/src/main/java/com/example/makoto_o/multiactivity/FirstActivity.java
+++ MultiActivity/app/src/main/java/com/example/makoto_o/multiactivity/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.example.makoto_o.multiactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        Button btnNext = (Button) this.findViewById(R.id.button1);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
